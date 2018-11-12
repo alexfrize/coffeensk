9a613c8ffa87179fd9eda85aa379171eb65bf81b
Alexander Frize <alexander.frize@gmail.com>
2018-11-11 20:31:18 -0800

    Commented Link import

--- src/components/contacts.component.jsx
+++ src/components/contacts.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import _ from "lodash";
 import "./contacts.component.scss";
